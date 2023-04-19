521e8e9d9453495c3306abb3300da912c5b40a29
lesitoan <lesitoan.media2gmail.com>
2023-04-19 13:30:43 +0700

    hook_useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const courses = [
+  {
+    id: 1,
+    data: 'java'
+  },
+  {
+    id: 2,
+    data: 'python'
+  },
+  {
+    id: 3,
+    data: 'ruby'
+  },
+];
 
 function App() {
+  const [checked, setChecked] = useState([]);
+  const handleOnChange = (id) => {
+    const isCheked = checked.includes(id);
+    console.log(isCheked)
+    if(isCheked) {
+      setChecked(checked.filter((value) => value !== id))
+    } else {
+      setChecked([...checked, id])
+    } 
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {courses.map((value, index) => (
+        <div key={index}>
+          <input checked={checked.includes(value.id)} onChange={() => handleOnChange(value.id)} type="checkbox" />
+          <lable>{value.data}</lable>
+        </div>
+      ))
+      }
     </div>
-  );
-}
+  )
+};
 
 export default App;
